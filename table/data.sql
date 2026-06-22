58d79bd9f38177a81bae4e1e599ff8042c3253b2
Evan Nartea <evannartea@Mac.lan>
2026-06-22 18:08:52 +0100

    cuppla changes hehe

---
+++ table/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO athletes (name, sport, country, height, weight)
+    VALUES
+    ('Muhammad Ali', 'Boxing', 'USA', 191, 97),
+    ('Jon Jones', 'MMA', 'USA', 193, 93)
+    ('Michael Jordan', 'Basketball', 'USA', 198, 98),
+    ('Lionel Messi', 'Football', 'Argentina', 170, 67),
+    ('Michael Phelps', 'Swimming', 'USA', 193, 88),
+    ('Usain Bolt', 'Sprints', 'Jamaica', 195, 94),
+    ('Magnus Carlsen', 'Chess', 'Norway', 178, 85)
+    ;
---
+++ table/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE athletes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    sport TEXT NOT NULL,
+    country TEXT NOT NULL,
+    height REAL NOT NULL,
+    weight REAL NOT NULL
+);
