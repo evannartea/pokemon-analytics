9a58495f07f613878077fe39148bea40d391ca04
Evan Nartea <evannartea@Mac.lan>
2026-06-24 12:01:26 +0100

    data janitor

--- py/analysis.py
+++ py/analysis.py
@@ -1,13 +1,19 @@
-# what in tarnation?
-import pandas as pd 
-from pandasql import sqldf
+import pandas as pd
 
-df = pd.read_csv("athlete_events.csv")
+pokemon = pd.read_csv('data/pokemon.csv')
+pokedex = pd.read_csv('data/pokedex.csv')
 
-query = """
-SELECT *
-FROM df
-GROUP BY Event
-"""
+pokedex = pokedex.rename(columns={'name': 'Name'})
 
-print(sqldf(query))
+merged_df = pokemon.merge(
+    pokedex[['pokedex_number', 'Name']],
+    on='Name',
+    how='left'
+)
+
+df_clean = merged_df[
+    (merged_df['Generation'] < 5) &
+    (~merged_df['Name'].str.contains('Mega|Primal', na=False))
+    ]
+
+print(df_clean.to_string())
